f14d2d009db262d2569ba346584d495016407e3a
caelinsheahan <caelinsheahan@gmail.com>
2018-03-20 17:33:50 -0600

    server starting

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const http = require("http")
+const port = process.env.PORT
+
+var io = require('socket.io')()
+var Stopwatch = require('timer-stopwatch');
+var timer = new Stopwatch(600000)
+
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    timer.start()
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', timer.ms);
+    }, interval);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('vote1', function(msg){
+    io.emit('vote1', msg);
+  });
+  socket.on('vote2', function(msg){
+    io.emit('vote2', msg);
+  });
+  socket.on('response1', function(msg){
+    io.emit('response1', msg);
+  });
+  socket.on('response2', function(msg){
+    io.emit('response2', msg);
+  });
+  socket.on('topic', function(msg){
+    io.emit('topic', msg);
+  });
+});
+
+io.listen(process.env.PORT);
+console.log('listening on port2 ', port2);
