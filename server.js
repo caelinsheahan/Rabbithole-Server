b1631853c7e0c2e310b74c9fb8598530fa52ee8c
caelinsheahan <caelinsheahan@gmail.com>
2018-03-20 18:06:39 -0600

    heroku changes

--- server.js
+++ server.js
@@ -12,8 +12,21 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('vote1', function(msg){
+  io.emit('vote1', msg);
+});
+socket.on('vote2', function(msg){
+  io.emit('vote2', msg);
+});
+socket.on('response1', function(msg){
+  io.emit('response1', msg);
+});
+socket.on('response2', function(msg){
+  io.emit('response2', msg);
+});
+socket.on('topic', function(msg){
+  io.emit('topic', msg);
+});
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
