3a6162edce6819ec19484df6fecc24808ad42705
caelinsheahan <caelinsheahan@gmail.com>
2018-03-20 21:46:17 -0600

    greatly expanded features

--- server.js
+++ server.js
@@ -18,11 +18,29 @@ io.on('connection', (socket) => {
 socket.on('vote2', function(msg){
   io.emit('vote2', msg);
 });
+socket.on('vote3', function(msg){
+  io.emit('vote3', msg);
+});
+socket.on('vote4', function(msg){
+  io.emit('vote4', msg);
+});
+socket.on('vote5', function(msg){
+  io.emit('vote5', msg);
+});
 socket.on('response1', function(msg){
   io.emit('response1', msg);
 });
 socket.on('response2', function(msg){
   io.emit('response2', msg);
+});
+socket.on('response3', function(msg){
+  io.emit('response3', msg);
+});
+socket.on('response4', function(msg){
+  io.emit('response4', msg);
+});
+socket.on('response5', function(msg){
+  io.emit('response5', msg);
 });
 socket.on('topic', function(msg){
   io.emit('topic', msg);
